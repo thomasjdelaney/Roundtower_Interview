6a7c5f8224f06dce53ea6eebe072d756f7daad17
thomasjdelaney <thomas.delaney@posteo.net>
2021-01-15 22:21:26 +0000

    Started the real work on clean up. Added lots of useful functions. Need to check certain cases before moving on.

--- py/load_and_clean.py
+++ py/load_and_clean.py
@@ -15,6 +15,47 @@ np.set_printoptions(linewidth=shutil.get_terminal_size().columns-2)
 proj_dir = os.path.join(os.environ.get('HOME'), 'Roundtower_Interview')
 csv_dir = os.path.join(proj_dir, 'csv')
 
+def loadOpenCloseTimesCsv():
+	"""
+	For loading in the csv file that contains opening and closing times info.
+	Arguments:	None
+	Returns:	pandas DataFrame
+	"""
+	str_to_time_converter = lambda x:dt.datetime.strptime(x, '%H:%M:%S').time()
+	open_close = pd.read_csv(os.path.join(csv_dir, 'open_close.csv'),
+		converters={'FUT_TRADING_HRS':lambda x:'' if not x else x,
+			'TRADING_DAY_START_TIME_EOD':str_to_time_converter,
+			'TRADING_DAY_END_TIME_EOD':str_to_time_converter}, 
+		dtype={'Bloomberg Ticker':str}, index_col=0)
+	return open_close
+
+def getOpenCloseTimesForTicker(open_close, ticker):
+	"""
+	For getting the opening anc closing times for a given ticker.
+	Arguments:	open_close, pandas DataFrame
+				ticker, str
+	Returns:	open_close_times, list of pairs of datetime.time
+				num_sessions, int (1 or 2)
+	"""
+	str_to_time_converter = lambda x:dt.datetime.strptime(x, '%H:%M').time()
+	future_trading_hrs = open_close.loc[ticker]['FUT_TRADING_HRS']
+	has_future_session = future_trading_hrs != ''
+	if has_future_session:
+		has_two_sessions = future_trading_hrs.find('&') > -1
+		if has_two_sessions:
+			num_sessions = 2
+			str_sessions = [session.strip().split('-') for session in future_trading_hrs.split('&')]
+			open_close_times = [[str_to_time_converter(t) for t in session] for session in str_sessions]
+		else:
+			num_sessions = 1
+			open_close_times = [[str_to_time_converter(t)for t in future_trading_hrs.split('-')]]
+	else:
+		num_sessions = 1
+		opening_time = open_close.loc[ticker]['TRADING_DAY_START_TIME_EOD']
+		closing_time = open_close.loc[ticker]['TRADING_DAY_END_TIME_EOD']
+		open_close_times = [[opening_time, closing_time]]
+	return open_close_times, num_sessions
+
 def printFileInfo(csv_file):
 	"""
 	For printing some information about the given csv file. Assumes that the index of the file is a datetime.
@@ -56,13 +97,13 @@ def getAllTimes():
 	"""
 	return np.array([dt.time(hour, minute) for hour, minute in product(range(24), range(60))])
 
-def plotInstrumentTimeProfile(bid_ask_counts, bid_col_name, ask_col_name, instrument_name, num_weekdays):
+def plotTickerTimeProfile(bid_ask_counts, bid_col_name, ask_col_name, ticker, num_weekdays):
 	"""
 	For plotting the number of bids/asks we have per minute of the day, per weekday
 	Arguments:	bid_ask_counts, pandas dataFrame
 				bid_col_name, str
 				ask_col_name, str
-				instrument_name, str
+				ticker, str
 				num_weekdays, int, the total number of weekdays for which we might have data
 	Returns:	nothing
 	"""
@@ -79,31 +120,78 @@ def plotInstrumentTimeProfile(bid_ask_counts, bid_col_name, ask_col_name, instru
 	ask_ax.set_ylabel('Num. of Asks', fontsize='large')
 	[tick.set_rotation(30) for tick in ask_ax.get_xticklabels()]
 	[ax.spines[s].set_visible(False) for ax,s in product(axes, ['top', 'right'])]
-	bid_ax.set_title(instrument_name + ', Total weekdays = ' + str(num_weekdays), fontsize='large')
+	bid_ax.set_title(ticker + ', Total weekdays = ' + str(num_weekdays), fontsize='large')
 	plt.tight_layout()
 
-def getInstrumentTimeProfile(instrument_name, bid_ask_frame):
+def getTickerTimeProfile(ticker, bid_ask_frame):
 	"""
 	For visualising the times at which we have bids and asks for a given instrument.
-	Arguments:	instrument_name, str
+	Arguments:	ticker, str
 				bid_ask_frame, pandas dataFrame
 	Returns:	displays figures showing distributions of bid/ask times
 	"""
-	bid_col_name = instrument_name + '_Bid'
-	ask_col_name = instrument_name + '_Ask'
+	bid_col_name, ask_col_name = getTickerBidAskColNames(ticker)
 	bid_ask_frame = bid_ask_frame[[bid_col_name, ask_col_name]].notna()
 	bid_ask_frame['time_of_day'] = bid_ask_frame.index.time
 	bid_ask_counts = bid_ask_frame.groupby('time_of_day').aggregate('sum')
 	frame_dates = getDataDatesFromFrame(bid_ask_frame)
 	num_weekdays = np.sum([date.weekday() < 5 for date in frame_dates])
-	plotInstrumentTimeProfile(bid_ask_counts, bid_col_name, ask_col_name, instrument_name, num_weekdays)
+	plotTickerTimeProfile(bid_ask_counts, bid_col_name, ask_col_name, instrument_name, num_weekdays)
 
-#for csv_file in csv_files:
-#	printFileInfo(csv_file)
+def getTickerBidAskColNames(ticker):
+	"""
+	For getting the bid and ask column names from a ticker.
+	Arguments: 	ticker, str
+	Returns:	bid_col_name, str
+				ask_col_name, str
+	"""
+	bid_col_name = ticker + '_Bid'
+	ask_col_name = ticker + '_Ask'
+	return bid_col_name, ask_col_name
 
+def cleanTickerBidsAsks(bid_ask_series, bid_ask_dates, open_close_times, num_sessions):
+	"""
+	For cleaning the up the bid and ask data. We want to fill forward bids and asks during a trading session.
+	But we don't to insert data on days where there is none (holidays, weekends).
+	We don't want to insert bids or asks before the first bid or quote of the session either.
+	Profile this code because it may be repeated often.
+	Arguments:	bid_ask_series, pandas DataFrame
+				bid_ask_dates, array of datetime.date
+				open_close_times, list of opening and closing times
+				num_sessions, the number of trading sessions for that ticker.
+	Returns:	pandas DataSeries
+	"""
+	# need to check that open time < close time
+	is_one_session = num_sessions == 1
+	if is_one_session:
+		open_time, close_time = open_close_times[0]
+		is_single_day_session = open_time < close_time
+		if is_single_day_session:
+			for date in bid_ask_dates:
+				date_bid_ask_series = bid_ask_series[bid_ask_series.index.date == date]
+				is_all_null_date = np.invert(date_bid_ask_series.notna().any())
+				trading_bid_ask_series = date_bid_ask_series[(date_bid_ask_series.index.time > open_time) 
+															& (date_bid_ask_series.index.time < close_time)]
+				filled_trading_bid_ask_series = trading_bid_ask_series.ffill()
+		else:
+			for date in bid_ask_dates:
+				date_bid_ask_series = bid_ask_series[bid_ask_series.index.date == date]
+
+
+open_close = loadOpenCloseTimesCsv()
 csv_files = glob.glob(os.path.join(csv_dir,'*'))
-csv_file = csv_files[0]
-bid_ask_frame = pd.read_csv(csv_file, parse_dates=[0], index_col=0)
-contract_names = extractContractNameFromColumns(bid_ask_frame.columns)
-[getInstrumentTimeProfile(con, bid_ask_frame) for con in contract_names[:3]]
-plt.show() # TODO save instead of show
+csv_file = csv_files[1]
+bid_ask_all_frame = pd.read_csv(csv_file, parse_dates=[0], index_col=0)
+bid_ask_dates = getDataDatesFromFrame(bid_ask_all_frame)
+ticker_names = extractContractNameFromColumns(bid_ask_all_frame.columns)
+ticker = 'GC1 Comdty' # we need special rules for some tickers, check Dan's email regularly
+open_close_times, num_sessions = getOpenCloseTimesForTicker(open_close, ticker)
+bid_col_name, ask_col_name = getTickerBidAskColNames(ticker)
+bid_ask_series = bid_ask_all_frame[bid_col_name]
+
+
+
+#[getInstrumentTimeProfile(ticker, bid_ask_frame) for ticker in ticker_names[:3]]
+#plt.show() # TODO save instead of show
+
+# TODO: tickers: GC1 Comdty, TP1 Index
